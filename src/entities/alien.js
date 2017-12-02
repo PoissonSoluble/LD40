3e2f15dc52e5edb09c72d2973186d73e848811ce
Gabon Pierre <pierre.gabon@ecole.ensicaen.fr>
2017-12-02 15:10:05 +0100

    entrée du vaisseau gérée

--- src/entities/alien.js
+++ src/entities/alien.js
@@ -2,20 +2,28 @@ class Alien extends Phaser.Sprite{
 
 	constructor(game, vaisseau, filons) {
 		super(game, 100, 100,  "alien");
+		this.anchor.setTo(0.5);
 		this.capacite = 5;
 		this.nbCristaux = 0;
 		this.cible = null;
 		this.tempsClonage = 0;
 		this.vaisseau = vaisseau;
 		this.filons = filons;
+
+		this.vaisseau.pushAlien(this);
 	}
 
 
 	update() {
 		if(this.cible) {
+			this.visible = true;
 			let angle = Phaser.Math.angleBetween(this.x, this.y, this.cible.x, this.cible.y);
 			this.x+=Math.cos(angle);
 			this.y+=Math.sin(angle);
+		}
+		else
+		{
+			this.visible = false; 
 		}
 		
 	}
@@ -31,8 +39,9 @@ class Alien extends Phaser.Sprite{
 		let cristauxAMiner = Math.min(cible.quantite, this.capacite)
 		for (let i = 0; i<cristauxAMiner; i++)
 		{
-			setTimeout(()=> {
-					this.nbCristaux+=this.filons.prelever(cible,1);
+			setTimeout(()=> 
+			{
+				this.nbCristaux+=this.filons.prelever(cible,1);
 
 			}, 1000);
 		}
@@ -41,6 +50,13 @@ class Alien extends Phaser.Sprite{
 	}
 
 
+
+	entrerVaisseau(){
+
+		this.vaisseau.pushAlien(this);
+		this.cible = null;
+
+	}
 
 
 
