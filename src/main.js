87f0e78a15668b3aba24c42bad9ca5f25d68ba68
Pellegrini Yann <ypellegrini@ecole.ensicaen.fr>
2017-12-02 12:31:53 +0100

    base

---
+++ src/main.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    let game = new Game;
+    window.game = game; // pour le debug lol!
+
+});
---
+++ src/states/load.js
@@ -0,0 +1,32 @@
+class LoadState extends Phaser.State {
+
+
+    preload() {
+        this.game.stage.backgroundColor = '#87e33a';
+        this.loaded = false;
+
+        // let logo = this.game.add.sprite(this.game.width / 2, this.game.height / 2, 'logo');
+        // logo.anchor.setTo(0.5,0.5);
+        // logo.width = logo.height = Math.min(this.game.width, this.game.height);
+
+        this.interval = setInterval(LoadState.prototype.checkIfLoaded.bind(this), 500);
+
+
+        this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        // this.game.load.image("carlier", "assets/carlier.png");
+        // this.game.load.image("blue", "assets/blue.png");
+       
+    }
+
+    checkIfLoaded() {
+        if(this.loaded) {
+            clearInterval(this.interval);
+            this.game.state.start('play');
+        }
+    }
+
+    create() {
+        this.loaded = true;
+    }
+
+}
