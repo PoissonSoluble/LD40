3b49c338eb231cba7243cfc5708d0273a1114a2a
Gabon Pierre <pierre.gabon@ecole.ensicaen.fr>
2017-12-02 17:44:02 +0100

    cristaux plus gros

--- src/entities/cristal.js
+++ src/entities/cristal.js
@@ -1,7 +1,7 @@
 class Cristal extends Filon {
 	constructor(game, x, y, vaisseau){
 		super(game, x, y, "cristal", vaisseau);
-		this.quantite = 1;
+		this.quantite = Math.random() * (50 - 1) + 1;
 		this.aliensPositionX -= 10;
 	}
 }
