faea1f6f3ed50ae6e7b8d65e4ce7c3e31ef7b25b
duplessis <duplessis@ensicaen.fr>
2017-12-02 20:18:29 +0100

    plein de trucs mais en fait pas tant que ça

--- src/entities/cristal.js
+++ src/entities/cristal.js
@@ -2,7 +2,7 @@ class Cristal extends Filon {
 	constructor(game, x, y, vaisseau){
 		super(game, x, y, "cristal", vaisseau);
 
-		this.quantite = ~~( Math.random() * (50 - 1) + 1);
+		this.quantite = this.game.rnd.integerInRange(1, 10);
 		this.aliensPositionX -= 10;
 		this.scale.setTo(2)
 	}
